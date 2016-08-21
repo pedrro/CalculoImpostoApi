44d21debf213a57ab244b7c4e566fcb25f845b01
pedro silva <pedrro2@hotmail.com>
2016-08-21 14:20:06 -0300

    criando diretorios para cara serviço

---
+++ imposto/src/main/java/imposto/api/ImpostoApplication.java
@@ -0,0 +1,12 @@
+package imposto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostoApplication.class, args);
+	}
+}
