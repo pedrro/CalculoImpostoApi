24027c4d73a58a6b35edf95f4fe65ffeb9cce879
William Ahrons <william.ahrons@gmail.com>
2016-08-22 10:21:46 -0300

    Modificando controller apra ficar mais visual

--- usuario/src/main/java/usuario/api/UsuarioController.java
+++ usuario/src/main/java/usuario/api/UsuarioController.java
@@ -17,38 +17,43 @@ import usuario.api.UsuarioRepository;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @RestController
+@RequestMapping(value = "/usuario")
 public class UsuarioController {
     @Autowired
     UsuarioRepository usuarioRepository;
     Usuario usuario;
 
-    @RequestMapping(value = "/usuario", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Usuario> criaUsuario(@RequestBody Usuario novoUsuario) {
-         usuario = new Usuario(novoUsuario.nome, novoUsuario.salario);
+        usuario = new Usuario(novoUsuario.nome, novoUsuario.salario);
         usuarioRepository.save(usuario);
-        return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
+        return new ResponseEntity<Usuario>(usuario, CREATED);
     }
 
-    @RequestMapping(value = "/usuario", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Usuario>> retornaUsuarios() {
-        return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), OK);
     }
 
-    @RequestMapping(value="/usuario/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}", method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Usuario> retornaUsuario(@PathVariable("id") UUID id) {
-        return new ResponseEntity<Usuario>(usuarioRepository.findOne(id), HttpStatus.FOUND);
+        return new ResponseEntity<Usuario>(usuarioRepository.findOne(id), FOUND);
     }
 
-    @RequestMapping(value= "/usuario/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "{id}", method = DELETE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Usuario> deletaUsuario(@PathVariable("id") UUID id) {
         try {
             usuario = usuarioRepository.findOne(id);
             usuarioRepository.delete(id);
         } catch (MongoException mongo) {
             System.out.println(mongo);
-            return new ResponseEntity<Usuario>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<Usuario>(INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<Usuario>(usuario,HttpStatus.ACCEPTED);
+        return new ResponseEntity<Usuario>(usuario, NO_CONTENT);
     }
 }
